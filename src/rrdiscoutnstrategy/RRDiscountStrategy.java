242dad9124a388f0a409baf24ff86503cefdf5b3
ritu <ritu@10.30.11.232>
2016-02-23 15:10:03 -0600

    RRDiscountStrategy - first commit

---
+++ src/rrdiscoutnstrategy/RRDiscountStrategy.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rrdiscoutnstrategy;
+
+/**
+ *
+ * @author ritu
+ */
+public class RRDiscountStrategy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // KLUDGE: Do configuration here
+        
+        
+        // Start talking to objects
+        
+        
+    }
+    
+}
